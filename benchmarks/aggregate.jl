ea9ac5800df0e6bcabceb76694816f412ecc8baa
jeremie <jeremie.desgagne.bouchard@gmail.com>
2024-01-31 14:01:20 -0500

    scores aggregation

--- benchmarks/aggregate.jl
+++ benchmarks/aggregate.jl
@@ -20,18 +20,18 @@ data_names = [
     "higgs/raw"
 ]
 
-
 data_name = data_names[6]
 
 # metric_vars = ["mse", "gini"]
 metric_vars = ["logloss", "accuracy"]
 # metric_vars = ["mse", "ndcg"]
 metric_var = first(metric_vars)
-var_names = ["model_type", "train_time", "best_nround", metric_vars...]
+# var_names = ["model_type", "train_time", "best_nround", metric_vars...]
+var_names = ["model_type", "train_time", metric_vars...]
 var_nums = ["train_time", metric_vars...]
 
 df = DataFrame()
-for algo in algos
+for algo in model_types
     path = joinpath(@__DIR__, "../results/$data_name/$algo.csv")
     _df = CSV.read(path, DataFrame)
     select!(_df, var_names)
@@ -41,5 +41,5 @@ end
 
 transform!(groupby(df, :model_type), metric_var => ordinalrank => :rank)
 subset!(groupby(df, :model_type), :rank => (x -> x .== minimum(x)))
-select!(df, :model_type, :best_nround, var_nums .=> (x -> round.(x, sigdigits=3)) .=> var_nums)
+select!(df, :model_type, var_nums .=> (x -> round.(x, sigdigits=3)) .=> var_nums)
 pretty_table(df; backend=Val(:markdown), show_subheader=false, alignment = :c)
