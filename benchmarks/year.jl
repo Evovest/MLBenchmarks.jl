f756942755bef7f2d271d01c02ddb3236bc432c3
jeremie.desgagne.bouchard <jeremie.desgagne.bouchard@gmail.com>
2025-04-08 17:44:23 -0400

    up

--- benchmarks/year.jl
+++ benchmarks/year.jl
@@ -44,7 +44,7 @@ _std = std(dtrain[!, target_name])
 dtrain.target_norm = (dtrain[!, target_name] .- _mean) ./ _std
 deval.target_norm = (deval[!, target_name] .- _mean) ./ _std
 
-hyper_list = MLBenchmarks.get_hyper_neurotrees(; loss=:mse, metric=:mse, device=:gpu, nrounds=200, early_stopping_rounds=2, lr=1e-3, ntrees=[32, 64, 128], stack_size=[1], depth=[3, 4, 5], hidden_size=[8, 16, 32], init_scale=0.01, batchsize)
+hyper_list = MLBenchmarks.get_hyper_neurotrees(; loss=:mse, metric=:mse, device=:gpu, nrounds=200, early_stopping_rounds=2, lr=1e-3, ntrees=[32, 64, 128], stack_size=[1], depth=[3, 4, 5], hidden_size=[8, 16, 32], init_scale=0.1, batchsize)
 hyper_list = sample(hyper_list, hyper_size, replace=false)
 
 results = Dict{Symbol,Any}[]
